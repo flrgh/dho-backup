ab5fffd99c07967db7f391e397f43459c91f4743
Michael Martin <michael.joe.martin@gmail.com>
2013-09-03 09:43:19 -0700

    first commit

---
+++ backupFunctions.py
@@ -0,0 +1,99 @@
+import hashlib
+import os
+import time
+from collections import namedtuple
+import subprocess
+from Crypto.Cipher import AES
+
+File = namedtuple('File', ['filename', 'basedir', 'size', 'encrypt'], verbose=False, rename=False)
+
+
+
+def get_file_list(backupDirectory, baseDirectory, encrypted):
+    
+    filelist = []
+    for root, subFolders, files in os.walk(backupDirectory):
+        for file in files:
+            fname = os.path.join(root,file)
+
+            f = File(
+                filename = fname,
+                basedir = baseDirectory,
+                size = os.path.getsize(fname),
+                encrypt = encrypted
+            )
+
+            filelist.append(f)
+
+    return filelist
+
+
+def logit(message, logfile):
+    l = open(logfile, 'a')    
+    l.write('{the_time}: {the_message}\n'.format(the_time=time.strftime('%H:%M:%S'), the_message=message))
+    l.close()
+
+
+def file_md5(filename):
+    md5 = hashlib.md5()
+    with open(filename,'rb') as f: 
+        for chunk in iter(lambda: f.read(8192), b''): 
+             md5.update(chunk)
+    return md5.hexdigest()
+
+
+
+def encrypt_file(file, key):
+    # key = hashlib.sha256(phrase).digest()    
+
+    pass
+
+def decrypt_file(file, key):
+    decryptor = AES.new(key, mode)
+    plain = decryptor.decrypt(ciphertext)
+    
+    pass
+
+
+def back_it_on_up(files, dho_bucket, logFile, skipSize=1073741824):
+    
+    bucket_contents = [key.name for key in dho_bucket.list()]
+    failed = []
+    skipped = []
+    success = []
+
+    for file in files:
+        shortName = file.filename[len(file.basedir)+1:]
+        
+        if not shortName.decode('utf8') in bucket_contents and file.size < skipSize:
+            
+            logit('Uploading: ' + file.filename, logFile)
+            
+            try:
+                key = dho_bucket.new_key(shortName)
+                key.set_contents_from_filename(file.filename)
+                key.set_canned_acl('private')
+
+                logit('Success! Uploaded ' + file.filename, logFile)
+                success.append(file.filename)
+            
+            except KeyboardInterrupt:
+                logit('User skipped {filename}'.format(filename=file.filename), logFile)
+                print '\nKeyboard interrupt--skipping {filename}'.format(filename=file.filename)
+
+            except Exception as e:
+                logit('FAILURE: ' + e.strerror, logFile)
+                failed.append(file.filename)
+
+        else:
+            logit('Skipping {filename}'.format(filename=file.filename), logFile)
+            skipped.append(file.filename)
+
+    return failed, skipped, success
+
+
+def run_bash(command):
+    result = subprocess.Popen(command.split(), stdout=subprocess.PIPE).stdout.read()
+    if result.endswith('\n'):
+        result = result[:-1]
+    return result
---
+++ uploader.py
@@ -0,0 +1,55 @@
+import time
+import dho
+from config import backupList, logFile, phrase
+from backupFunctions import *
+
+startTime = time.time()
+logit('Backup starting...', logFile)
+
+
+
+
+totals = [0, 0, 0,]
+
+for item in backupList:
+    print item
+    logit('Backing up {directory} to bucket \'{bucket}\' now.'.format(directory=item['directory'], bucket=item['bucket']), logFile)
+
+    files_for_backup = get_file_list(backupDirectory=item['directory'], baseDirectory=item['basedir'], encrypted=item['encrypt'])
+    logit('This directory contains a total of {number} files... backing up now'.format(number=len(files_for_backup)), logFile)
+
+    bucket = dho.conn.get_bucket(item['bucket'])
+    failed, skipped, success = back_it_on_up(files_for_backup, bucket, logFile)
+
+
+    finished = 'Done with {directory}. Files backed up: {success}, Files skipped: {skipped}, Failures: {failed}, Total: {summ}.'.format(
+        directory=item['directory'],
+        success=len(success),
+        skipped=len(skipped),
+        failed=len(failed),
+        summ=(len(success)+len(skipped)+len(failed))
+    )
+
+    delim = '\n' + ('-'*90)
+
+    logit(finished + delim, logFile)
+
+    totals[0] += len(success)
+    totals[1] += len(skipped)
+    totals[2] += len(failed)
+
+
+backupTime = round(((time.time() - startTime)/60.0), 2)
+
+print 'Aaaand we\'re done here. Some stats:\n'
+
+stats = '\n\tBackup time: {time} minutes\n\tFiles skipped: {skipped}\n\tFiles Failed: {failed}\n\tFiles uploaded: {uploaded}\n\tTotal: {total}'.format(
+    time=backupTime,
+    skipped=totals[1],
+    failed=totals[2],
+    uploaded=totals[0],
+    total = sum(totals)
+    )
+
+print stats
+logit(stats, logFile)
