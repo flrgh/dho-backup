c803fd6a5f09d7fe0c5136dc40bc37ec0ebb9741
Michael Martin <michael.joe.martin@gmail.com>
2013-11-24 20:09:06 -0800

    reworking the update script

--- shortToLongUpdate.py
+++ shortToLongUpdate.py
@@ -17,17 +17,21 @@ def rename_long_to_short(bz):
     
     for k in bucket.list():
     
-        new_name = os.path.join(bz.basedir, k.name)                
+        bd = bz.basedir[1:]
    
-        if not os.path.exists(new_name):
-            print 'WARNING object, {file} does not exist'.format(file=new_name)
+#        if not os.path.exists(new_name):
+#            print 'WARNING object, {file} does not exist'.format(file=new_name.encode('utf-8', 'ignore'))
             
         if not TESTING:
-            print 'RENAMING key to {n}'.format(n=new_name.encode('ascii', 'ignore'))
-            k.copy(k.bucket.name, new_name)
-            k.delete()
-
-
+            if not k.name.startswith(bd):
+                new_name = bd + '/' + k.name
+                print 'RENAMING key to {n}'.format(n=new_name.encode('utf-8', 'ignore'))
+                k.copy(k.bucket.name, new_name)
+                k.delete()
+            elif k.name.startswith(bd + '/' + bd):
+                print 'Fixing the stupid rename problem'
+                k.copy(k.bucket.name, k.name.replace(bd+'/'+bd, bd))
+                k.delete()
            
 
 for backup_object in backupList:
