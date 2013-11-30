ae7eb24b0d4fbcecb481f8002c7c0b9add3112fb
Michael Martin <michael.joe.martin@gmail.com>
2013-11-29 17:11:18 -0800

    config file changes

--- config.example.py
+++ config.example.py
@@ -15,16 +15,18 @@ secret_key = 'SECRET KEY'
 # No trailing slashes!
 backupList = (
     {
-           'directory' : '/path/to/backup1',    # We'll backup everything in this directory
-              'bucket' : 'bucketname',          # The name of the bucket to upload files to
-             'encrypt' : False,                 # Do you want to encrypt files before uploading?
-      'skip_filetypes' : 'mp3, doc'             # Ignore certain file types
+             'directory' : '/path/to/backup1',    # We'll backup everything in this directory
+                'bucket' : 'bucketname',          # The name of the bucket to upload files to
+               'encrypt' : False,                 # Do you want to encrypt files before uploading?
+      'skip_directories' : ['/some/directory'],   # Skip some directories
+      'ignore_filetypes' : ['mp3', 'docx']        # Ignore certain file types
     },
     {
-            'directory': '/path/to/encrypted/backup2',
-               'bucket': 'bucketname2',
-              'encrypt': True,
-       'skip_filetypes': ''
+             'directory' : '/path/to/encrypted/backup2',
+                'bucket' : 'bucketname2',
+               'encrypt' : True,
+      'skip_directories' : [],
+      'ignore_filetypes' : []
     }
 )
 
